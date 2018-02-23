48c3fd8f0677eaaf2d38f7cebcd33e1a36be1fa2
Geoffrey Copin <copin.geoffrey@gmail.com>
2018-02-23 22:59:11 +0100

    Amélioration readme + modifs exporter

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 puck2
 =====
 
+Configuration avec IntelliJ
+---------------------------
+1. Créer un nouveau projet depuis les sources.
+2. Activer l'option **Delegate IDE build/run actions to gradle** dans  
+`Preferences -> Buid, Execution, Deployment -> Build tools -> Gradle -> Runner`
+
 Compilation
 -----------
 `./gradlew`
 
 Usage
 -----
-`java -jar puck2 programDir ?outputFile`
+`java -jar puck2.jar programDir ?outputFile`
--- src/main/java/graph/XMLExporter.java
+++ src/main/java/graph/XMLExporter.java
@@ -28,27 +28,28 @@ public class XMLExporter {
     public String generateXml() {
         StringBuilder builder = new StringBuilder("<?xml version=\"1.0\"?>\n");
         builder.append("<DependencyGraphe>\n");
-        builder.append("\t<Nodes>\n");
+        builder.append("\t<nodes>\n");
 
         for (Node n: nodes.values()) {
             builder.append(nodeToString(n));
         }
 
-        builder.append("\t</Nodes>\n");
-        builder.append("\t<Edges>\n");
+        builder.append("\t</nodes>\n");
+        builder.append("\t<edges>\n");
 
+        int currentEdgeIndex = 0;
         for (Edge e: edges) {
-            builder.append(edgeToString(e));
+            builder.append(edgeToString(e, currentEdgeIndex++));
         }
 
-        builder.append("\t</Edges>\n");
+        builder.append("\t</edges>\n");
         builder.append("</DependencyGraphe>\n");
 
         return builder.toString();
     }
 
     private String nodeToString(Node node) {
-        String formatString = "\t\t<Node type=\"%s\" id=\"%d\" name=\"%s\"/>\n";
+        String formatString = "\t\t<node type=\"%s\" id=\"%d\" name=\"%s\"/>\n";
         String type = node.getType().toString().toLowerCase();
         Integer id = node.getId();
         String name = extractNodeName(node.getFullName());
@@ -63,12 +64,12 @@ public class XMLExporter {
         return nameParts[nameParts.length - 1];
     }
 
-    private String edgeToString(Edge edge) {
-        String formatString = "\t\t<Edge type=\"%s\" src=\"%d\" dest=\"%s\"/>\n";
+    private String edgeToString(Edge edge, int id) {
+        String formatString = "\t\t<edge type=\"%s\" src=\"%d\" dest=\"%s\" id=\"%d\"/>\n";
         String type = edge.getType().toString().toLowerCase();
         Integer src = nodes.get(edge.getSourceName()).getId();
         Integer dest = nodes.get(edge.getTargetName()).getId();
 
-        return String.format(formatString, type, src, dest);
+        return String.format(formatString, type, src, dest, id);
     }
 }
