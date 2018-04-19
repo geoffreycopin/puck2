fb323888e7dacfb98b5b81bc4990f9a397f3b8ef
Geoffrey Copin <copin.geoffrey@gmail.com>
2018-04-19 10:45:31 +0200

    Ajout de tests pour les methodBody

---
+++ testdir/uses_methodbody/Test.java
@@ -0,0 +1,16 @@
+public class Point {
+    public int x;
+    public int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
+
+public class Test {
+    public void foo() {
+        Point p = new Point(1, 1);
+        int x = p.x;
+        p.setX(2);
+    }
+}
